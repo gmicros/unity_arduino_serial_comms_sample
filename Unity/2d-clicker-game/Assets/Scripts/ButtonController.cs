efbd27ab5033ff6cb49ae11dd84b1e820651236b
gcm <george.micros@gmail.com>
2020-09-28 21:04:11 -0400

    initial commit of Unity project

---
+++ Unity/2d-clicker-game/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using gmicros_arduino_interface;
+
+public class ButtonController : MonoBehaviour
+{
+    public Vibrotactor vibro;
+    public int duration;
+    public int intensity;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    public void TaskOnClick()
+    {
+        ArduinoSerialInterface.Init();
+        string msg = ArduinoSerialInterface.FillMessage(vibro, duration, intensity);
+        Debug.Log("Filled msg: " + msg);
+        ArduinoSerialInterface.SendMessage(msg);        
+    }
+}
