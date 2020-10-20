5422c8c2172486189b4e2c114fb667bd51177d8a
gcm <george.micros@gmail.com>
2020-10-19 22:17:38 -0400

    parse command message on arduino

--- Arduino/SerialRepeat/SerialRepeat.ino
+++ Arduino/SerialRepeat/SerialRepeat.ino
@@ -21,6 +21,11 @@
 String inputString = "";         // a String to hold incoming data
 bool stringComplete = false;  // whether the string is complete
 
+String identifier = "UNITY";
+String start_delimiter = "$";
+String field_delimiter = ",";
+String checksum_delimiter = "*";
+
 void setup() {
   // initialize serial:
   Serial.begin(9600);
@@ -34,6 +39,72 @@ void loop() {
 
 }
 
+// $UNITY,Front,0,6,*30
+bool parseCommandString(String command){
+  String sub = command.substring(0,1);
+  sub + "\n\r";
+  //Serial.write(sub.c_str());
+  if(command.substring(0,1) != start_delimiter){
+    Serial.write("could not find start_delimiter\n");    
+    return false;
+  }
+  if(command.substring(1,6) != identifier){
+    Serial.write("could not find id\n");    
+    return false;
+  }
+
+  int ind = 6;
+  char buf[200];
+  command.toCharArray(buf, sizeof(buf));
+  char *p = &buf[7];
+  char* str;
+  int num_tokens = 0;
+  String resp ="";
+  while( (str = strtok_r(p, ",",  &p)) != NULL){
+    //Serial.write(str);
+    switch(num_tokens)
+    {
+      case 0:
+      {
+      // vibrotactor
+      resp += " vibrotactor: " + String(str);
+      break;
+      }
+      case 1:
+      {
+        // duration
+        int duration = atoi(str);
+        resp += " duration: " + String(str);
+        break;
+      }
+      case 2:
+      {
+        // intensity
+        int intensity = atoi(str);
+        resp += " intensity: " + String(str);
+        break;
+      }
+      case 3:
+      {
+        // checksum
+        int checksum = atoi(str+1);
+        resp += " checksum: " + String(str+1);
+        break;
+      }
+    } 
+    
+    num_tokens++;
+  }
+  resp += "\n";
+
+  //TODO(gmicros): verify checksum 
+
+  //TODO(gmicros): command vibrotactor 
+  Serial.write(resp.c_str());
+  
+
+  return false;
+}
 /*
   SerialEvent occurs whenever a new data comes in the hardware serial RX. This
   routine is run between each time loop() runs, so using delay inside loop can
@@ -49,7 +120,10 @@ void serialEvent() {
     // do something about it:
     if (inChar == '\n') {
       stringComplete = true;
+      bool val = parseCommandString(inputString);
+      if(val){
       Serial.write(inputString.c_str());
+      }
       inputString = "";
     }
   }
