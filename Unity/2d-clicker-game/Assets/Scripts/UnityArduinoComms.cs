391e56613e46a96cf43d2767b9d2c28307e7fe35
gcm <george.micros@gmail.com>
2020-10-21 20:14:03 -0400

    documenting the C# code

--- Unity/2d-clicker-game/Assets/Scripts/UnityArduinoComms.cs
+++ Unity/2d-clicker-game/Assets/Scripts/UnityArduinoComms.cs
@@ -2,13 +2,28 @@
 using System.Text;
 using System.IO.Ports;
 
+/// <summary>
+/// defines classes for serial communication between Unity and Arduino
+/// </summary>
 namespace UnityArduinoComms
 {
-
+    /// <summary>
+    /// API class to communicate with Arduino
+    /// </summary>
     public static class ArduinoSerialInterface
     {
-        private static bool init = false;
+        /// <summary>
+        /// serial port object used for communication
+        /// </summary>
         private static SerialPort port_ = null;
+        /// <summary>
+        /// initialization flag to track object state
+        /// </summary>
+        private static bool init = false;
+
+        /// <summary>
+        /// Init() creates a new port and initialize it's properties for communication over the serial port
+        /// </summary>
         public static void Init()
         {
             if (port_ == null)
@@ -28,13 +43,22 @@ namespace UnityArduinoComms
             }
         }
 
+        // TODO(gmicros): there needs to be a way to close the port, destructor/cleanup
+
+        /// <summary>
+        /// Send a framed message over the serial port to the Arduino
+        /// </summary>
+        /// <param name="message">a string containing a framed message</param>
+        /// <returns>the response</returns>
         public static string SendMessage(string message)
         {
+            // init uninitialized object
             if (!init)
             {
                 Init();
             }
 
+           
             if (port_.IsOpen)
             {
                 try
@@ -42,6 +66,8 @@ namespace UnityArduinoComms
                     port_.Write(message);
                     try
                     {
+                        // read the respose
+                        // TODO(gmicros): handle the reponse, check for valid
                         string resp = port_.ReadLine();
                         return resp;
                         //Debug.Log("resp: " + resp);
@@ -60,6 +86,9 @@ namespace UnityArduinoComms
         }
     }
 
+    /// <summary>
+    /// Vibrotactor enumeration class
+    /// </summary>
     public enum Vibrotactor
     {
         Front,
@@ -70,6 +99,9 @@ namespace UnityArduinoComms
         Bottom
     }
 
+    /// <summary>
+    /// tokens used in message framing
+    /// </summary>
     static class MessageStructure
     {
         internal static string identifier = "UNITY";
@@ -78,8 +110,18 @@ namespace UnityArduinoComms
         internal static char field_delimiter_ = ',';
     }
 
+    /// <summary>
+    /// functions used to frame messages
+    /// </summary>
     public class MessageUtils
     {
+        /// <summary>
+        /// Frame a message with the values passed
+        /// </summary>
+        /// <param name="vib">vibrotactor to activate</param>
+        /// <param name="duration">duration of stumulus</param>
+        /// <param name="intensity">intensity of stimulus</param>
+        /// <returns>the framed message</returns>
         public static string FillMessage(Vibrotactor vib, int duration, int intensity)
         {
             string msg_body = FillMessageBody(vib, duration, intensity);
@@ -87,6 +129,13 @@ namespace UnityArduinoComms
             return msg;
         }
 
+        /// <summary>
+        /// Frame the message body with the values passed
+        /// </summary>
+        /// <param name="vib">vibrotactor to activate</param>
+        /// <param name="duration">duration of stimulus</param>
+        /// <param name="intensity">intensity of stimulus</param>
+        /// <returns>the delimited message body</returns>
         internal static string FillMessageBody(Vibrotactor vib, int duration, int intensity)
         {
             string msg_body = "";
@@ -97,6 +146,10 @@ namespace UnityArduinoComms
             return msg_body;
         }
 
+        /// <summary>
+        /// Create a test message for troubleshooting
+        /// </summary>
+        /// <returns>the framed test message</returns>
         public static string FillTestMessage()
         {
             string msg = "GPRMC,092751.000,A,5321.6802,N,00630.3371,W,0.06,31.66,280511,,,A";
@@ -105,6 +158,12 @@ namespace UnityArduinoComms
             msg += MessageStructure.checksum_delimiter_ + check.ToString("X2");
             return msg;
         }
+
+        /// <summary>
+        /// Prepend the header and append the checksum to the framed message body
+        /// </summary>
+        /// <param name="msg_body">delimited message body</param>
+        /// <returns>framed message with valid checksum</returns>
         internal static string AddHeaderAndChecksum(string msg_body)
         {
             int check = checksum(msg_body);
@@ -112,10 +171,16 @@ namespace UnityArduinoComms
             return msg;
         }
 
+        /// <summary>
+        /// Generate checksum by XORing all the bytes
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <returns>checksum of message</returns>
         internal static int checksum(string msg)
         {
             byte[] msg_bytes = Encoding.ASCII.GetBytes(msg);
             int checksum = 0;
+            // XOR all the bytes together
             foreach (byte c in msg_bytes)
             {
                 checksum ^= c;
