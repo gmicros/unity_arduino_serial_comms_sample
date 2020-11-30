d190d98dfdee26e7a699c648099301b2e128eaf7
gcm <george.micros@gmail.com>
2020-11-29 19:32:06 -0500

    removed dupe Arduino project

---
+++ ArduinoSerialInterface/CommsTestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityArduinoComms;
+
+namespace CommsTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test_msg = MessageUtils.FillTestMessage();
+            string resp = ArduinoSerialInterface.SendMessage(test_msg);
+            Console.WriteLine("response: ", resp);
+        }
+    }
+}
